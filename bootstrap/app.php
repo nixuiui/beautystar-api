392310b6526ae62a3829970daef8af8088c73337
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2019-11-30 12:37:46 +0700

    add mail configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -80,6 +80,17 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(\Illuminate\Mail\MailServiceProvider::class);
+
+/*
+|--------------------------------------------------------------------------
+| Load The Configure Mail
+|--------------------------------------------------------------------------
+*/
+$app->configure('mail');
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
 
 /*
 |--------------------------------------------------------------------------
